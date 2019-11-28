5e29b1b77e405102fdaec199e473eb4e85527322
ManonMic <manon.michelet5@gmail.com>
2019-11-28 17:23:51 -0500

    Permits to crop an image around the detected card.

--- shape_recognition.py
+++ shape_recognition.py
@@ -1,21 +1,39 @@
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 import imutils
-import argparse
 
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
-args = vars(ap.parse_args())
-
-image = cv2.imread(args["image"])
+image = cv2.imread("carte.jpg")
 image_resized = imutils.resize(image, width=400)
-cv2.imshow('image',image_resized)
+
+gray = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(gray, (1, 1), 1000)
+thresh = cv2.threshold(blur, 180, 200, cv2.THRESH_BINARY)[1]
+
+edged = cv2.Canny(thresh, 100, 200)
+
+output = image_resized.copy()
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
+
+for c in contours:
+    cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
+    cv2.imshow("Contours", output)
+    cv2.waitKey(0)
+
+text = "I found {} cards!".format(len(contours))
+cv2.putText(output, text, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+            (240, 0, 159), 2)
+cv2.imshow("Contours", output)
 cv2.waitKey(0)
 
-images_gray = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray", gray)
-cv2.waitKey(0)
+rects = []
+
+for c in contours:
+    perimeter = cv2.arcLength(c, True)
+    approximation = cv2.approxPolyDP(c, 0.02 * perimeter, True)
+    x, y, w, h = cv2.boundingRect(approximation)
+    cropped = image_resized[y:y + h, x:x + w]
+    cv2.imshow("cropped", cropped)
+    cv2.waitKey(0)
 
 cv2.destroyAllWindows()
